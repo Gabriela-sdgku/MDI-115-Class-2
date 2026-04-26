2d3706325c8d34cfd6878e6d4be55b888bc2ebea
Gabriela-sdgku <gabriela.sanchez@sdgku.edu>
2026-04-26 13:16:03 -0700

    Add files via upload

---
+++ Students ToDo Task/ToDo Task/ContentView.swift
@@ -0,0 +1,101 @@
+//
+//  ContentView.swift
+//  ToDo Task
+//
+//  Created SDGKU
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var taskGroups: [TaskGroup] = []
+    @State private var selectedGroup: TaskGroup? // selected group
+    @State private var columnVisibility: NavigationSplitViewVisibility = .all // navigation side panel
+    @State private var isShowingAddGroup = false
+    @Environment(\.scenePhase) private var scenePhase
+    @AppStorage("isDarkMode") private var isDarkMode = false
+    let saveKey = "savedTaskGroups"
+    @Environment(\.dismiss) private var dismiss
+    @Binding var profile: Profile
+    
+    var body: some View {
+        NavigationSplitView(columnVisibility: $columnVisibility) {
+            // SIDEBAR
+            List(selection: $selectedGroup) {
+                ForEach(profile.groups) {group in
+                    NavigationLink(value: group) {
+                        Label(group.title, systemImage: group.symbolName)
+                    }
+                }
+            }
+            .navigationTitle(profile.name)
+            .listStyle(.sidebar)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Image(systemName:"chevron.left")
+                    }
+                }
+                
+                ToolbarItem(placement: .primaryAction) {
+                    Button {
+                        isShowingAddGroup = true
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                }
+            }
+        } detail: {
+            if let group = selectedGroup {
+                if let index = profile.groups.firstIndex(where: { $0.id == group.id }) {
+                    TaskGroupDetailView(groups: $profile.groups[index])
+                }
+            } else {
+                ContentUnavailableView("Select a Group", systemImage: "sidebar.left")
+            }
+        }
+        .navigationSplitViewStyle(.balanced)
+        .navigationBarBackButtonHidden(true)
+        .sheet(isPresented: $isShowingAddGroup) {
+            NewGroupView { newGroup in
+                profile.groups.append(newGroup)
+            }
+        }
+        .onAppear {
+            loadData()
+        }
+        .onChange(of: scenePhase) { oldValue, newValue in
+            if newValue == .active {
+                print("🟢 App is Active")
+            } else if newValue == .inactive {
+                print("🟡 App is Inactive")
+            } else if newValue == .background {
+                print("🔴 App is Background - Saving Data!")
+                saveData()
+            }
+        }
+        .preferredColorScheme(isDarkMode ? .dark : .light)
+
+    }
+    
+    func saveData() {
+        if let encodedData = try? JSONEncoder().encode(profile.groups){
+            UserDefaults.standard.set(encodedData, forKey: saveKey)
+        }
+    }
+    
+    func loadData() {
+        if let savedData = UserDefaults.standard.data(forKey: saveKey){
+            if let decodedGrpups = try? JSONDecoder().decode([TaskGroup].self, from: savedData) {
+                profile.groups = decodedGrpups
+                return
+            }
+        }
+        // show mock data for dev purposes
+        if profile.groups.isEmpty {
+            profile.groups = TaskGroup.sampleData
+        }
+    }
+}
---
+++ Students ToDo Task/ToDo Task/TaskModels.swift
@@ -0,0 +1,50 @@
+//
+//  TaskModels.swift
+//  ToDo Task
+//
+//  Created by Gabriela Sanchez on 09/12/25.
+//
+
+import Foundation
+
+struct TaskItem: Identifiable, Hashable, Codable {
+    var id = UUID()
+    var title: String
+    var isCompleted: Bool = false
+}
+
+struct TaskGroup: Identifiable, Hashable, Codable {
+    var id = UUID()
+    var title: String
+    var symbolName: String
+    var tasks: [TaskItem]
+}
+
+struct Profile: Identifiable, Hashable, Codable {
+    var id = UUID()
+    var name: String
+    var profileImage: String
+    var groups: [TaskGroup]
+}
+
+// MOCK DATA
+extension TaskGroup {
+    static let sampleData: [TaskGroup] = [
+        TaskGroup(title: "Groceries", symbolName: "storefront.circle.fill", tasks: [
+            TaskItem(title: "Buy Apples"),
+            TaskItem(title: "Buy Milk")
+        ]),
+        
+        TaskGroup(title: "Home", symbolName: "house.fill", tasks: [
+            TaskItem(title: "Walk the dog", isCompleted: true ),
+            TaskItem(title: "Clean the kitchen")
+        ])
+    ]
+}
+
+extension Profile {
+    static let sample: [Profile] = [
+        Profile(name: "Professor", profileImage: "professor_img", groups: TaskGroup.sampleData),
+        Profile(name: "Student", profileImage: "student_img", groups: [])
+    ]
+}
